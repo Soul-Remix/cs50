315e920fee7c236b9cf93f043edbd74bcccd06e1
Soul-Remix <soulremix-webdev@pm.me>
2021-11-19 12:31:32 +0200

    pset4 filter project

---
+++ pset4/filter/helpers.c
@@ -0,0 +1,113 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float blue = image[i][j].rgbtBlue;
+            float green = image[i][j].rgbtGreen;
+            float red = image[i][j].rgbtRed;
+
+            int average = round((blue + green + red) / 3);
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float blue = image[i][j].rgbtBlue;
+            float green = image[i][j].rgbtGreen;
+            float red = image[i][j].rgbtRed;
+
+            float sepiaRed = .393 * red + .769 * green + .189 * blue;
+            float sepiaGreen = .349 * red + .686 * green + .168 * blue;
+            float sepiaBlue = .272 * red + .534 * green + .131 * blue;
+
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtRed = sepiaRed;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE tmp = image[i][j];
+
+            image[i][j] = image[i][width - j];
+            image[i][width - j] = tmp;
+        }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE tmp[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            tmp[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sum_b = 0;
+            float sum_g = 0;
+            float sum_r = 0;
+            int c = 0;
+
+            for (int k = -1; k < 2; k++)
+            {
+                for (int l = -1; l < 2; l++)
+                {
+                    // Check if pixel is outside rows
+                    if (i + k < 0 || i + k >= height)
+                    {
+                        continue;
+                    }
+                    // Check if pixel is outside columns
+                    if (j + l < 0 || j + l >= width)
+                    {
+                        continue;
+                    }
+                    // Otherwise add to sums
+                    sum_b += tmp[i + k][j + l].rgbtBlue;
+                    sum_g += tmp[i + k][j + l].rgbtGreen;
+                    sum_r += tmp[i + k][j + l].rgbtRed;
+                    c++;
+                }
+            }
+            // Get average and blur image
+            image[i][j].rgbtBlue = round(sum_b / c);
+            image[i][j].rgbtGreen = round(sum_g / c);
+            image[i][j].rgbtRed = round(sum_r / c);
+        }
+    }
+
+    return;
+}
